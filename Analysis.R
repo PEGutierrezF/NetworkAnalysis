57f3a8c0e4dd55105ac42e6d4b573b081916968b
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2021-05-07 10:44:31 -0600

    data 2020

--- Analysis.R
+++ Analysis.R
@@ -40,7 +40,7 @@ graph.density(network2000)
 # 2020 --------------------------------------------------------------------
 ###########################################################################
 
-# MC, LJ, AR 20, LJ 29
+# MC, AR 20, LJ 29-39, LFL 34, VCP 36, DJ 40, ED 44, TS 45 53, JH 45 49, VSS 45, DKP 53
 
 df2020<- read.csv("autor2020.csv")
 df2020a <- df2020 %>% select(V1, V2)
@@ -50,11 +50,52 @@ df2020nodes<- read.csv("autor2020nodes.csv")
 df2020b <- df2020nodes %>% select(Authors, Country)
 df2020b
 
-network2020 <- graph_from_data_frame(d=df2020a, vertices=df2020b, directed=F) # covert in a igraph
+network2020 <- graph_from_data_frame(df2020a, vertices=df2020b, directed=F) # covert in a igraph
+network2020[]
+plot.igraph(network2020)
 
-plot(network2020)
 
-plot(network2020, edge.arrow.size=.2, edge.color="black", #line color
+
+
+
+ceb <- cluster_edge_betweenness(network2020) 
+
+dendPlot(ceb, mode="hclust")
+plot(ceb, network2020,
+     vertex.label=NA) 
+
+
+
+
+lay <- layout_with_drl(network2020, options=list(simmer.attraction=0))
+
+plot(network2020, layout = lay,
+     vertex.size = 2, vertex.label.cex = 0.9,
+     vertex.label.color = 'black', vertex.label.family = 'arial',
+     vertex.label.dist = 0.2, vertex.frame.color = 'white',
+     edge.arrow.size = 0.2, edge.curved = TRUE)
+
+
+
+
+ggraph(network2020, 'igraph', algorithm = 'tree') + 
+        geom_edge_link() +
+        ggforce::theme_no_axes()
+
+ggraph(network2020, 'igraph', algorithm = 'tree', circular = TRUE) + 
+        geom_edge_diagonal(aes(alpha = ..index..)) +
+        coord_fixed() + 
+        scale_edge_alpha('Direction', guide = 'edge_direction') +
+        geom_node_point(aes(filter = degree(network2020, mode = 'out') == 0), 
+                        color = 'steelblue', size = 1) +
+        ggforce::theme_no_axes()
+
+ggraph(network2020, 'igraph', algorithm = 'tree') + 
+        geom_edge_link() + 
+        geom_node_point()
+
+
+plot.igraph(network2020, edge.arrow.size=.5, edge.color="black", #line color
      vertex.color="lightblue", vertex.frame.color="lightblue", # bubble color
      vertex.label=V(network2020)$media, vertex.label.color="black") # letters
 
