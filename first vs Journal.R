162bbf91b8759ee17e99152d356261719f53400e
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2022-07-17 22:36:44 -0400

    data

--- first vs Journal.R
+++ first vs Journal.R
@@ -27,7 +27,6 @@ n.2003 <- read_excel(path = network.data, sheet = "2003nodes")
 n.2004 <- read_excel(path = network.data, sheet = "2004nodes")
 n.2005 <- read_excel(path = network.data, sheet = "2005nodes")
 
-
 network.data <- "data/data2006_2010.xlsx"
 excel_sheets(path = network.data)
 
@@ -42,11 +41,28 @@ excel_sheets(path = network.data)
 
 n.2011 <- read_excel(path = network.data, sheet = "2011nodes")
 n.2012 <- read_excel(path = network.data, sheet = "2012nodes")
+n.2013 <- read_excel(path = network.data, sheet = "2013nodes")
+n.2014 <- read_excel(path = network.data, sheet = "2014nodes")
+n.2015 <- read_excel(path = network.data, sheet = "2015nodes")
 
-df.journals <- rbind(n.2000, n.2001, n.2002, n.2003, n.2004, n.2005,
-                     n.2006, n.2007, n.2008, n.2009, n.2010, n.2011,
-                     n.2012)
+network.data <- "data/data2016_2020.xlsx"
+excel_sheets(path = network.data)
 
+n.2016 <- read_excel(path = network.data, sheet = "2016nodes")
+n.2017 <- read_excel(path = network.data, sheet = "2017nodes")
+n.2018 <- read_excel(path = network.data, sheet = "2018nodes")
+#n.2019 <- read_excel(path = network.data, sheet = "2019nodes")
+#n.2020 <- read_excel(path = network.data, sheet = "2020nodes")
+
+network.data <- "data/data2000_2020.xlsx" # add single author paper
+excel_sheets(path = network.data) # add single author paper
+
+n.2000_20 <- read_excel(path = network.data, sheet = "2000-20nodes")
+
+df.t <- rbind(n.2000, n.2001, n.2002, n.2003, n.2004, n.2005,
+              n.2006, n.2007, n.2008, n.2009, n.2010, n.2011,
+              n.2012, n.2013, n.2014, n.2015, n.2016, n.2017,
+              n.2018, n.2000_20)
 
 
 # Latin American as first Authors -----------------------------------------
